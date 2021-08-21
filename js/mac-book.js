b4006a2f25de22870698c16e86190712b4f1965c
itsfaarhaad <itsfaarhaad@gmail.com>
2021-08-21 15:30:46 +0100

    code minimization complete using function

--- js/mac-book.js
+++ js/mac-book.js
@@ -1,135 +1,103 @@
+// declaring function for memory buttons
+function getMemoryTotal(extraMemory) {
+    const bestPrice = document.getElementById('best-price');
+    bestPrice.innerText = 1299;
+    const extraStorageField = document.getElementById('extra-storage-price');
+    const extraStoragePrice = extraStorageField.innerText;
+    const deliveryChargeField = document.getElementById('delivery-charge');
+    const deliveryChargeCost = deliveryChargeField.innerText;
+
+    const extraMemoryField = document.getElementById('extra-memory-price');
+    extraMemoryField.innerText = extraMemory;
+    const extraMemoryPrice = extraMemoryField.innerText;
+
+    const totalPrice = document.getElementById('total-price');
+    totalPrice.innerText = parseInt(extraMemoryPrice) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice) + parseInt(deliveryChargeCost);
+    const footerTotal = document.getElementById('footer-total');
+    footerTotal.innerText = totalPrice.innerText;
+
+    return totalPrice.innerText;
+}
+// event handling for 8GB Memory button
 document.getElementById('memory-8gb').addEventListener('click', function () {
-    const bestPrice = document.getElementById('best-price');
-    bestPrice.innerText = 1299;
-    const extraStorageField = document.getElementById('extra-storage-price');
-    const extraStoragePrice = extraStorageField.innerText;
-    const deliveryChargeField = document.getElementById('delivery-charge');
-    const deliveryChargeCost = deliveryChargeField.innerText;
-
-    const extraMemoryField = document.getElementById('extra-memory-price');
-    extraMemoryField.innerText = 0;
-    const extraMemoryPrice = extraMemoryField.innerText;
-
     const totalPrice = document.getElementById('total-price');
-    totalPrice.innerText = parseInt(extraMemoryPrice) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice) + parseInt(deliveryChargeCost);
-    const footerTotal = document.getElementById('footer-total');
-    footerTotal.innerText = parseInt(extraMemoryPrice) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice) + parseInt(deliveryChargeCost);
+    totalPrice.innerText = getMemoryTotal(0);
 })
-
+// event handling for 16GB Memory button
 document.getElementById('memory-16gb').addEventListener('click', function () {
+    const totalPrice = document.getElementById('total-price');
+    totalPrice.innerText = getMemoryTotal(180);
+})
+
+// declaring function for storage buttons
+function getStorageTotal(extraStorage) {
     const bestPrice = document.getElementById('best-price');
     bestPrice.innerText = 1299;
-    const extraStorageField = document.getElementById('extra-storage-price');
-    const extraStoragePrice = extraStorageField.innerText;
+    const extraMemoryField = document.getElementById('extra-memory-price');
+    const extraMemoryPrice = extraMemoryField.innerText;
     const deliveryChargeField = document.getElementById('delivery-charge');
     const deliveryChargeCost = deliveryChargeField.innerText;
 
-    const extraMemoryField = document.getElementById('extra-memory-price');
-    extraMemoryField.innerText = 180;
-    const extraMemoryPrice = extraMemoryField.innerText;
+    const extraStorageField = document.getElementById('extra-storage-price');
+    extraStorageField.innerText = extraStorage;
+    const extraStoragePrice = extraStorageField.innerText;
 
     const totalPrice = document.getElementById('total-price');
-    totalPrice.innerText = parseInt(extraMemoryPrice) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice) + parseInt(deliveryChargeCost);
+    totalPrice.innerText = parseInt(extraStoragePrice) + parseInt(bestPrice.innerText) + parseInt(extraMemoryPrice) + parseInt(deliveryChargeCost);
     const footerTotal = document.getElementById('footer-total');
-    footerTotal.innerText = parseInt(extraMemoryPrice) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice) + parseInt(deliveryChargeCost);
-})
-// event handling for 256GB SSD button
+    footerTotal.innerText = totalPrice.innerText;
+
+    return totalPrice.innerText;
+}
+// event handling for 256GB SSD storage
 document.getElementById('ssd-256gb').addEventListener('click', function () {
-    const bestPrice = document.getElementById('best-price');
-    bestPrice.innerText = 1299;
-    const extraMemoryField = document.getElementById('extra-memory-price');
-    const extraMemoryPrice = extraMemoryField.innerText;
-    const deliveryChargeField = document.getElementById('delivery-charge');
-    const deliveryChargeCost = deliveryChargeField.innerText;
-
-    const extraStorageField = document.getElementById('extra-storage-price');
-    extraStorageField.innerText = 0;
-    const extraStoragePrice = extraStorageField.innerText;
-
     const totalPrice = document.getElementById('total-price');
-    totalPrice.innerText = parseInt(extraStoragePrice) + parseInt(bestPrice.innerText) + parseInt(extraMemoryPrice) + parseInt(deliveryChargeCost);
-    const footerTotal = document.getElementById('footer-total');
-    footerTotal.innerText = parseInt(extraMemoryPrice) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice) + parseInt(deliveryChargeCost);
+    totalPrice.innerText = getStorageTotal(0);
 })
 
-// storage buttons handling
+// event handling for 512GB SSD storage
 document.getElementById('ssd-512gb').addEventListener('click', function () {
-    const bestPrice = document.getElementById('best-price');
-    bestPrice.innerText = 1299;
-    const extraMemoryField = document.getElementById('extra-memory-price');
-    const extraMemoryPrice = extraMemoryField.innerText;
-    const deliveryChargeField = document.getElementById('delivery-charge');
-    const deliveryChargeCost = deliveryChargeField.innerText;
-
-    const extraStorageField = document.getElementById('extra-storage-price');
-    extraStorageField.innerText = 100;
-    const extraStoragePrice = extraStorageField.innerText;
-
     const totalPrice = document.getElementById('total-price');
-    totalPrice.innerText = parseInt(extraStoragePrice) + parseInt(bestPrice.innerText) + parseInt(extraMemoryPrice) + parseInt(deliveryChargeCost);
-    const footerTotal = document.getElementById('footer-total');
-    footerTotal.innerText = parseInt(extraMemoryPrice) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice) + parseInt(deliveryChargeCost);
+    totalPrice.innerText = getStorageTotal(100);
 })
 
-// event handling for 1TB SSD
+// event handling for 1TB SSD storage
 document.getElementById('ssd-1tb').addEventListener('click', function () {
+    const totalPrice = document.getElementById('total-price');
+    totalPrice.innerText = getStorageTotal(180);
+})
+
+// declaring function for delivary charge buttons
+function getDeliveryCost(deliveryCost) {
+    const deliveryChargeField = document.getElementById('delivery-charge');
+    deliveryChargeField.innerText = deliveryCost;
+    const deliveryChargeCost = deliveryChargeField.innerText;
 
     const bestPrice = document.getElementById('best-price');
     bestPrice.innerText = 1299;
+    const extraStorageField = document.getElementById('extra-storage-price');
+    const extraStoragePrice = extraStorageField.innerText;
     const extraMemoryField = document.getElementById('extra-memory-price');
     const extraMemoryPrice = extraMemoryField.innerText;
-    const deliveryChargeField = document.getElementById('delivery-charge');
-    const deliveryChargeCost = deliveryChargeField.innerText;
-
-    const extraStorageField = document.getElementById('extra-storage-price');
-    extraStorageField.innerText = 180;
-    const extraStoragePrice = extraStorageField.innerText;
 
     const totalPrice = document.getElementById('total-price');
-    totalPrice.innerText = parseInt(extraStoragePrice) + parseInt(bestPrice.innerText) + parseInt(extraMemoryPrice) + parseInt(deliveryChargeCost);
-    // totalPrice.innerText = parseInt(extraMemoryPrice.innerText) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice.innerText) + parseInt(deliveryChargeCost.innerText);
+    totalPrice.innerText = parseInt(extraMemoryPrice) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice) + parseInt(deliveryChargeCost);
     const footerTotal = document.getElementById('footer-total');
-    footerTotal.innerText = parseInt(extraMemoryPrice) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice) + parseInt(deliveryChargeCost);
-})
+    footerTotal.innerText = totalPrice.innerText;
 
-// event handlig for delivery cost button free delivery
+    return totalPrice.innerText;
+}
+// event handling for free delivery button
 document.getElementById('free-delivery').addEventListener('click', function () {
-    const deliveryChargeField = document.getElementById('delivery-charge');
-    deliveryChargeField.innerText = 0;
-    const deliveryChargeCost = deliveryChargeField.innerText;
-
-    const bestPrice = document.getElementById('best-price');
-    bestPrice.innerText = 1299;
-    const extraStorageField = document.getElementById('extra-storage-price');
-    const extraStoragePrice = extraStorageField.innerText;
-    const extraMemoryField = document.getElementById('extra-memory-price');
-    const extraMemoryPrice = extraMemoryField.innerText;
-
     const totalPrice = document.getElementById('total-price');
-    totalPrice.innerText = parseInt(extraMemoryPrice) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice) + parseInt(deliveryChargeCost);
-    const footerTotal = document.getElementById('footer-total');
-    footerTotal.innerText = parseInt(extraMemoryPrice) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice) + parseInt(deliveryChargeCost);
+    totalPrice.innerText = getDeliveryCost(0);
 })
 
-// event handlig for delivery cost button fast delivery
+// event handlig for fast delivery button
 document.getElementById('fast-delivery').addEventListener('click', function () {
-    const deliveryChargeField = document.getElementById('delivery-charge');
-    deliveryChargeField.innerText = 20;
-    const deliveryChargeCost = deliveryChargeField.innerText;
-
-    const bestPrice = document.getElementById('best-price');
-    bestPrice.innerText = 1299;
-    const extraStorageField = document.getElementById('extra-storage-price');
-    const extraStoragePrice = extraStorageField.innerText;
-    const extraMemoryField = document.getElementById('extra-memory-price');
-    const extraMemoryPrice = extraMemoryField.innerText;
-
     const totalPrice = document.getElementById('total-price');
-    totalPrice.innerText = parseInt(extraMemoryPrice) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice) + parseInt(deliveryChargeCost);
-    const footerTotal = document.getElementById('footer-total');
-    footerTotal.innerText = parseInt(extraMemoryPrice) + parseInt(bestPrice.innerText) + parseInt(extraStoragePrice) + parseInt(deliveryChargeCost);
+    totalPrice.innerText = getDeliveryCost(20);
 })
-
 
 // Promo Code Section
 document.getElementById('promo-apply').addEventListener('click', function () {
@@ -143,15 +111,11 @@ document.getElementById('promo-apply').addEventListener('click', function () {
     const totalPrice = parseInt(totalPriceField.innerText);
 
     const percent = 20 / 100 * footerTotal;
+    // comparing the promoCode
     if (promoCodeValue == 'stevekaku') {
         const total = totalPrice - percent;
         footerTotalField.innerText = total;
     }
-    // else {
-    //     const p = document.createElement('p');
-    //     p = 'Please insert a valid promo code';
-    //     const promo = document.getElementById('promo');
-    //     promo.appendChild('p');
-    // }
+    // clearing the promo code input value
     promoCodeField.value = '';
 })
